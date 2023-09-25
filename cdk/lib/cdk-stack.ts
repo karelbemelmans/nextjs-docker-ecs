7d909e3b5882fc9658621dc3982c57ebcca49652
Karel Bemelmans <karel@narfum.org>
2023-09-25 18:15:49 +0200

    WIP

---
+++ cdk/lib/cdk-stack.ts
@@ -0,0 +1,29 @@
+import * as cdk from "aws-cdk-lib";
+import * as ecs from "aws-cdk-lib/aws-ecs";
+import * as ecsPatterns from "aws-cdk-lib/aws-ecs-patterns";
+import { Construct } from "constructs";
+
+export class CdkStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const loadBalancedFargateService =
+      new ecsPatterns.ApplicationLoadBalancedFargateService(this, "Service", {
+        publicLoadBalancer: true,
+        redirectHTTP: true,
+        memoryLimitMiB: 512,
+        cpu: 256,
+        healthCheckGracePeriod: cdk.Duration.seconds(5),
+        taskImageOptions: {
+          image: ecs.ContainerImage.fromRegistry(
+            "ghcr.io/karelbemelmans/nextjs-docker:main"
+          ),
+          containerPort: 3000,
+        },
+      });
+
+    loadBalancedFargateService.targetGroup.configureHealthCheck({
+      path: "/api/hello",
+    });
+  }
+}
--- infra/README.md
+++
@@ -1,3 +0,0 @@
-# Reference
-
-https://dev.to/alexvladut/how-to-add-aws-cdk-to-an-existing-project-2d30
